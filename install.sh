9b606318ab2e919d74a0352da0083e4d1ea92fdf
Scott Francis <scott.russell.francis@gmail.com>
2019-11-18 21:46:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Installing wifi reset service to /opt/wifi-reset."
-mkdir -p /opt/wifi-reset
-cp -f wifi-reset.sh /opt/wifi-reset/wifi-reset.sh
+echo "Installing koo-reset service to /opt/koo-reset."
+mkdir -p /opt/koo-reset
+cp -f koo-reset.sh /opt/koo-reset/koo-reset.sh
 echo "Installing systemd service to run at boot."
-cp -f wifi-reset.service /lib/systemd/system/wifi-reset.service
+cp -f koo-reset.service /lib/systemd/system/koo-reset.service
 echo "Enabling systemd service."
-systemctl enable wifi-reset.service > /dev/null
+systemctl enable koo-reset.service > /dev/null
